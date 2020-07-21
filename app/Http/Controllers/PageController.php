19d8f516df422c4e788dad77ae2b8c156dd3a4dd
Erditya Lintang Alifansyah <lintangalifansyah01@gmail.com>
2020-07-21 20:21:38 +0700

    Initial commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {    
+    return view('Page.home');
+    }
+    public function product()
+    {    
+    return view('Page.product');
+    }
+}
---
+++ resources/views/Page/product.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layout')
+
+@section('subtitle', 'See Our Product Customer')
+
+@section('breadcrumb')
+    @parent
+    <a href="/">Home</a><a href="/product">/Product</a>
+@endsection
+
+@section('container')
+    Hello World!
+@endsection
